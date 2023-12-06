718cba723b9e56ed98e8d4208e7ccc9c06ab30a7
Alex <thealexspindler@gmail.com>
2023-12-06 15:33:23 -0800

    social_link_remove.js

---
+++ social_link_remove.js
@@ -0,0 +1,142 @@
+//use first to check if extension loading
+//change the color of the border of the entire body element
+document.body.style.border = "5px solid red";
+
+
+//SOCIAL MEDIA REMOVAL
+console.log("goodbye social links");
+
+//healthpartners.com
+const b = document.getElementsByClassName("social-media");
+for (let l=0; l< b.length; l++) {
+        if(b[l]) {
+                b[l].style.opacity = 0.0;
+        } else {}
+}
+
+//ucsd.edu
+const c = document.getElementsByClassName("footer-social-links");
+for (let l=0; l< c.length; l++) {
+        if(c[l]) {
+                c[l].style.opacity = 0.0;
+        } else {}
+}
+
+//bank of america
+const d = document.getElementsByClassName("boa-social");
+for (let l=0; l< d.length; l++) {
+        if(d[l]) {
+                d[l].style.opacity = 0.0;
+        } else {}
+}
+
+//ca.gov
+const e = document.getElementsByClassName("socialsharer-container");
+for (let l=0; l< e.length; l++) {
+        if(e[l]) {
+                e[l].style.opacity = 0.0;
+        } else {}
+}
+
+//ca.gov pt 2 also why is the home button a part of the social links?
+const f = document.getElementsByClassName("social-media-links");
+for (let l=0; l< f.length; l++) {
+        if(f[l]) {
+                f[l].style.opacity = 0.0;
+        } else {}
+}
+
+//hgtv.com
+//https://www.hgtv.com/outdoors/flowers-and-plants/houseplants
+const g = document.getElementsByClassName("o-SocialShare");
+for (let l=0; l< g.length; l++) {
+        if(g[l]) {
+                g[l].style.opacity = 0.0;
+        } else {}
+}
+
+//hgtv pt 2 newsletter banner
+const h = document.getElementsByClassName("o-NewsletterBanner");
+for (let l=0; l< h.length; l++) {
+        if(h[l]) {
+                h[l].style.opacity = 0.0;
+        } else {}
+}
+//hgtv pt 3 Go shopping/On TV/ Newsletter, Follow us
+const i = document.getElementsByClassName("container-aside");
+for (let l=0; l< i.length; l++) {
+        if(i[l]) {
+                i[l].style.opacity = 0.0;
+        } else {}
+}
+
+
+//ARM.com needs getElementsById
+
+//developer.arm.com
+const j = document.getElementsByClassName("c-navigation__contextual");
+for (let l=0; l< j.length; l++) {
+        if(j[l]) {
+                j[l].style.opacity = 0.0;
+        } else {}
+}
+
+//arm.com feedback button
+//developer.arm.com
+const k = document.getElementsByClassName("kampyle_vertical_button");
+for (let l=0; l< k.length; l++) {
+        if(k[l]) {
+                k[l].style.opacity = 0.0;
+        } else {}
+}
+
+//arm.com bottom socials
+const m = document.getElementsByClassName("c-footer__column-6");
+for (let l=0; l< m.length; l++) {
+        if(m[l]) {
+                m[l].style.opacity = 0.0;
+        } else {}
+}
+
+
+//penguinrandomhouse.com
+//takeover modal able to override somehow...
+const n = document.getElementsByClassName("takeover-modal");
+for (let l=0; l< n.length; l++) {
+        if(n[l]) {
+                n[l].style.opacity = 0.0;
+        } else {}
+}
+
+//pt 2 to get background of takeover modal 
+const o = document.getElementsByClassName("takeover-desktop-backdrop");
+for (let l=0; l< o.length; l++) {
+        if(o[l]) {
+                o[l].style.opacity = 0.0;
+        } else {}
+}
+
+//giant newsletter suscribe 
+const p = document.getElementsByClassName("subsc-box");
+for (let l=0; l< p.length; l++) {
+        if(p[l]) {
+                p[l].style.opacity = 0.0;
+        } else {}
+}
+
+//footer newsletter and socials
+const q = document.getElementsByClassName("desktop-show");
+for (let l=0; l< q.length; l++) {
+        if(q[l]) {
+                q[l].style.opacity = 0.0;
+        } else {}
+}
+
+//socials share
+const r = document.getElementsByClassName("ct-social");
+for (let l=0; l< r.length; l++) {
+        if(r[l]) {
+                r[l].style.opacity = 0.0;
+        } else {}
+}
+
